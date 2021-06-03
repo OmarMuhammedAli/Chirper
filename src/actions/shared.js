6f11b83a46af4ed63f440c3da33cc463cf401250
Omar <omarmuhammed1998.om@gmail.com>
2021-06-03 05:01:09 +0200

    added actions for different parts of the store

---
+++ src/actions/shared.js
@@ -0,0 +1,15 @@
+import {getInitialData} from '../utils/api'
+import {receiveUsers} from './users'
+import {receiveTweets} from './tweets'
+import {setAuthedUser} from './authedUsers'
+
+const AUTHED_USER = 'tylermcginnis'
+
+export const handleInitialData = () => (dispatch) => {
+    return getInitialData()
+        .then(({users, tweets}) => {
+            dispatch(receiveTweets(tweets))
+            dispatch(receiveUsers(users))
+            dispatch(setAuthedUser(AUTHED_USER))
+        })
+}
