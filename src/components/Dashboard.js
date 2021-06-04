330185381946e47e2b237527da1c8d2721d7461e
Omar <omarmuhammed1998.om@gmail.com>
2021-06-04 09:31:58 +0200

    promoted Dashboard component to a container

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
 
 class Dashboard extends Component {
     render() {
+        const {tweetIDs} = this.props
         return (
             <div>
-                
+                <h3 className='center'>Your Timeline</h3>
+                <ul className='dashboard-list'>
+                    {
+                        tweetIDs.map(id => (
+                            <li key={id}>{id}</li>
+                        ))
+                    }
+                </ul>
             </div>
         )
     }
